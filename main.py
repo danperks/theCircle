e989117271b62b6d11bee2925829f5ce0149d675
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-04-10 21:51:31 +0100

    Closed off sql statement

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ def AddNewShopRequest():
     if "shopID" in request.form:
         placeID = request.form["shopid"]
         parmas = {"t":tuple([placeID])}
-        SQLcursor.execute("INSERT INTO \"ShopRequests\"() VALUES)
+        SQLcursor.execute("INSERT INTO \"ShopRequests\"() VALUES")
     
     return "s"
 
