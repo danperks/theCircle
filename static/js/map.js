a713a0d1a4ac76e5b9dd499310ccc4510c8e5f24
MonocledMajesticProductions <46687573+MonocledMajesticProductions@users.noreply.github.com>
2020-04-11 11:02:23 +0100

    Search Bar on Map Browse Now Working

--- static/js/map.js
+++ static/js/map.js
@@ -94,3 +94,93 @@ function initMap() { //https://developers.google.com/maps/documentation/javascri
         
     });
 }
+
+
+
+
+function MapBrowse() { //https://developers.google.com/maps/documentation/javascript/geolocation
+    var map, infoWindow;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 51.841100, lng: -30.508039 },
+        zoom: 13,
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false
+    });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            map.setCenter(new google.maps.LatLng(pos.lat, pos.lng));
+        })
+    };
+
+
+
+
+
+
+
+
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    //Specify just the place data fields that you need.
+    autocomplete.setFields(['place_id', 'geometry', 'name']);
+
+    //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+
+    var marker = new google.maps.Marker({ map: map });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+        console.log("ins")
+    });
+    
+    autocomplete.addListener('place_changed', function() {
+        
+        infowindow.close();
+
+        var place = autocomplete.getPlace();
+
+        if (!place.geometry) {
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        // Set the position of the marker using the place ID and location.
+        marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+        });
+
+        marker.setVisible(true);
+
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-id'].textContent = place.place_id;
+        infowindowContent.children['place-address'].textContent =
+            place.formatted_address;
+        
+        ExportPlaceID = place.place_id;
+        console.log(ExportPlaceID)
+        
+        document.getElementById("hiddenplaceid").value =ExportPlaceID;
+        infowindow.open(map, marker);
+        
+    });
+}
